f9ab76e1a965bab9bb4f8ee6e15bf350c8161346
JuliMart <juli.martinezm@duocuc.cl>
2023-11-19 10:21:09 -0300

    muestraDatosDesdeStorage!

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -4,9 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FirebaseService {
-  getUsuarioData(): import("rxjs").Observable<any> {
-    throw new Error('Method not implemented.');
-  }
 
   constructor() { }
 }
